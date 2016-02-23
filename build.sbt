303c66c8ece059354290fe8303714d24ea4dc5fd
Felix Satyaputra <felix.satyaputra@typesafe.com>
2016-02-23 13:38:58 +1100

    Make it work with webjar locator

--- build.sbt
+++ build.sbt
@@ -3,6 +3,8 @@ name := "handlebars-nashorn-messing-about"
 enablePlugins(JavaServerAppPackaging)
 
 libraryDependencies ++= List(
+  "org.webjars" % "webjars-locator" % "0.30",
+  "org.webjars" % "handlebars" % "4.0.2",
   "org.scalatest" %% "scalatest" % "2.2.4"
 )
 
--- src/main/scala/foo/Hello.scala
+++ src/main/scala/foo/Hello.scala
@@ -2,12 +2,18 @@ package foo
 
 import java.io.{InputStreamReader, FileReader, File}
 import javax.script._
+import org.webjars.WebJarAssetLocator
+
 import scala.collection.JavaConverters._
 
 object Hello {
   val engineManager = new ScriptEngineManager(this.getClass.getClassLoader)
   val engine = engineManager.getEngineByName("nashorn")
-  val compiledHandlebars = compile(engine, "/handlebars-4.0.5.js")
+
+  val webJarAssetLocator = new WebJarAssetLocator(
+    WebJarAssetLocator.getFullPathIndex(""".*""".r.pattern, this.getClass.getClassLoader)
+  )
+  val compiledHandlebars = compile(engine, s"/${webJarAssetLocator.getFullPath("handlebars.js")}")
   val compiledScript = compile(engine, "/script.js")
 
   def main(args: Array[String]): Unit = {
